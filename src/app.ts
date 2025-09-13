5ebcac985d3c03f24a7ad49809115bf6bd90fd4f
xZephy <xayzexx@gmail.com>
2025-09-13 16:25:58 +0530

    Add test case

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req: express.Request, res: express.Response): void => {
+  res.status(200).json({
+    success: true,
+    message: "Hello world!",
+  });
+});
+
+app.listen(5500, () => console.log("Server listening on 5500 port"));
