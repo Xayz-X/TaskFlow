be12802f663a1c6c45f4e7dc18f705840e5f8186
xZephy <xayzexx@gmail.com>
2025-09-13 16:44:26 +0530

    Add default middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express from "express";
 import { PORT } from "./config/env";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.get("/", (req: express.Request, res: express.Response): void => {
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// health check ednpoint
+app.get("/health", (req: express.Request, res: express.Response): void => {
   res.status(200).json({
     success: true,
-    message: "Hello world!",
+    message: `Running on port: ${PORT}`,
   });
 });
 
+// start the server on the .env file defined port
 app.listen(PORT, () =>
   console.log(`Server listening on http://localhost:${PORT}`)
 );
